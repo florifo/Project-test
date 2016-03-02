c898cdfef2f90755215c622d60a57b46ee529873
Christianm <christianm@thehangar.cr>
2016-03-02 10:37:30 -0600

    Updating gulp file to enable ES6 based on the gulp file that Andres updated for One Hundred.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,6 +12,10 @@ import shell from 'shelljs';
 // BrowserSync is used to live-reload your website
 import browserSync from 'browser-sync';
 const reload = browserSync.reload;
+// Enable ES6
+import babelify from 'babelify';
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
 // AutoPrefixer
 import autoprefixer from 'autoprefixer';
 // Yargs for command line arguments
@@ -24,7 +28,7 @@ import {stream as wiredep} from 'wiredep';
 // 'gulp clean:gzip' -- erases all the gzipped files
 // 'gulp clean:metadata' -- deletes the metadata file for Jekyll
 gulp.task('clean:assets', () => {
-  return del(['.tmp/**/*', '!.tmp/assets', '!.tmp/assets/images', '!.tmp/assets/images/**/*', 'dist/assets']);
+  return del(['.tmp/**/*', '!.tmp/assets', '!.tmp/assets/images', '!.tmp/assets/images/**/*', 'dist/assets', 'dist/bower_components']);
 });
 gulp.task('clean:images', () => {
   return del(['.tmp/assets/images', 'dist/assets/images']);
@@ -82,7 +86,7 @@ gulp.task('styles', () =>
       showFiles: true
     })))
     .pipe($.if(argv.prod, $.rev()))
-    .pipe($.if(!argv.prod, $.sourcemaps.write('.')))
+    .pipe($.if(!argv.prod, $.sourcemaps.write()))
     .pipe($.if(argv.prod, gulp.dest('.tmp/assets/stylesheets')))
     .pipe($.if(argv.prod, $.if('*.css', $.gzip({append: true}))))
     .pipe($.if(argv.prod, $.size({
@@ -102,7 +106,6 @@ gulp.task('scripts', () =>
   // NOTE: The order here is important since it's concatenated in order from
   // top to bottom, so you want vendor scripts etc on top
   gulp.src([
-    'src/assets/javascript/vendor.js',
     'src/assets/javascript/main.js'
   ])
     .pipe($.newer('.tmp/assets/javascript/index.js', {dest: '.tmp/assets/javascript', ext: '.js'}))
@@ -131,6 +134,32 @@ gulp.task('scripts', () =>
     .pipe($.if(!argv.prod, browserSync.stream()))
 );
 
+var notifier = require('node-notifier');
+// Standard handler
+var standardHandler = function (err) {
+  // Notification
+  // var notifier = new notification();
+  notifier.notify({ message: 'Error: ' + err.message });
+  // Log to console
+  $.util.log($.util.colors.red('Error'), err.message);
+}
+
+// Es6 browserify and babel
+// enable module system
+gulp.task('es6', () =>
+
+  browserify({
+    entries: 'src/assets/javascript/es6/main.js',
+      debug: true
+    })
+    .transform(babelify)
+    .on('error', standardHandler)
+    .bundle()
+    .on('error', standardHandler)
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('src/assets/javascript/'))
+);
+
 // 'gulp inject:head' -- injects our style.css file into the head of our HTML
 gulp.task('inject:head', () =>
   gulp.src('src/_includes/head.html')
@@ -147,12 +176,36 @@ gulp.task('inject:footer', () =>
     .pipe(gulp.dest('src/_includes'))
 );
 
+// Combine svg files and inject it into pages
+gulp.task('svg', () => {
+    var svgs = gulp
+        .src('src/assets/images/svg/*.svg')
+        .pipe($.svgmin())
+        .pipe($.svgstore({ inlineSvg: true }));
+
+    function fileContents (filePath, file) {
+        return file.contents.toString();
+    }
+
+    return gulp
+        .src('src/_includes/svgs.html')
+        .pipe($.inject(svgs, { transform: fileContents }))
+        .pipe(gulp.dest('src/_includes'));
+});
+
 // 'gulp images' -- optimizes and caches your images
 gulp.task('images', () =>
   gulp.src('src/assets/images/**/*')
-    .pipe($.cache($.imagemin({
+    .pipe($.if($.if.isFile, $.cache($.imagemin({
       progressive: true,
-      interlaced: true
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    }))
+    .on('error', function (err) {
+      console.log(err);
+      this.end();
     })))
     .pipe(gulp.dest('.tmp/assets/images'))
     .pipe($.size({title: 'images'}))
@@ -215,33 +268,35 @@ gulp.task('serve', () => {
   // Watch various files for changes and do the needful
   gulp.watch(['src/**/*.md', 'src/**/*.html', 'src/**/*.yml'], gulp.series('jekyll', reload));
   gulp.watch(['src/**/*.xml', 'src/**/*.txt'], gulp.series('jekyll'));
-  gulp.watch('src/assets/javascript/**/*.js', gulp.series('scripts'));
+  gulp.watch('src/assets/javascript/es6/**/*.js', gulp.series('es6', 'scripts'));
   gulp.watch('src/assets/scss/**/*.scss', gulp.series('styles'));
-  gulp.watch('src/assets/images/**/*', reload);
+  gulp.watch('src/assets/images/svg/*.svg', gulp.series('svg'));
+  gulp.watch('src/assets/images/**/*', gulp.series('images', reload));
   gulp.watch('bower.json', gulp.series('wiredep', 'fonts'));
 });
 
 // inject bower components
-gulp.task('wiredep', () => {
+gulp.task('wiredep', () =>
   gulp.src('src/assets/scss/**/*.scss')
     .pipe(wiredep({
       ignorePath: /^(\.\.\/)+/
     }))
-    .pipe(gulp.dest('src/assets/scss'));
+    .pipe(gulp.dest('src/assets/scss')),
 
   gulp.src('src/_includes/scripts.html')
     .pipe(wiredep({
       ignorePath: /^(\.\.\/)*\.\./
     }))
-    .pipe(gulp.dest('src/_includes'));
-});
+    .pipe(gulp.dest('src/_includes'))
+);
 
 // 'gulp assets' -- cleans out your assets and rebuilds them
 // 'gulp assets --prod' -- cleans out your assets and rebuilds them with
 // production settings
 gulp.task('assets', gulp.series(
   gulp.series('clean:assets'),
-  gulp.parallel('styles', 'scripts', 'fonts', 'images')
+  gulp.series('es6'),
+  gulp.parallel('styles', 'scripts', 'fonts', 'images', 'svg')
 ));
 
 // 'gulp assets:copy' -- copies the assets into the dist folder, needs to be
@@ -262,16 +317,23 @@ gulp.task('default', gulp.series(
   gulp.series('serve')
 ));
 
+// Copy Bower Files
+gulp.task('bower:copy', () =>
+  gulp.src('bower_components/**/*')
+  .pipe(gulp.dest('.tmp/bower_components'))
+  .pipe(gulp.dest('dist/bower_components'))
+);
+
 // 'gulp build' -- same as 'gulp' but doesn't serve your site in your browser
 // 'gulp build --prod' -- same as above but with production settings
 gulp.task('build', gulp.series(
   gulp.series('clean:assets', 'clean:gzip'),
   gulp.series('assets', 'inject:head', 'inject:footer'),
-  gulp.series('jekyll', 'assets:copy', 'html')
+  gulp.series('jekyll', 'bower:copy', 'assets:copy', 'html')
 ));
 
 // 'gulp clean' -- erases your assets and gzipped files
-gulp.task('clean', gulp.series('clean:assets', 'clean:gzip'));
+gulp.task('clean', gulp.series('clean:assets', 'clean:images', 'clean:gzip'));
 
 // 'gulp rebuild' -- WARNING: Erases your assets and built site, use only when
 // you need to do a complete rebuild
